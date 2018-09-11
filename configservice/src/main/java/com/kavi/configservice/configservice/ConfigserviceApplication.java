7fbaa837beae84f6b9c19fe48622d0d31aa9d095
kaviarasu70 <kaviarasu70@gmail.com>
2018-09-10 20:43:58 -0700

    all in one service

---
+++ configservice/src/main/java/com/kavi/configservice/configservice/ConfigserviceApplication.java
@@ -0,0 +1,12 @@
+package com.kavi.configservice.configservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigserviceApplication.class, args);
+	}
+}
