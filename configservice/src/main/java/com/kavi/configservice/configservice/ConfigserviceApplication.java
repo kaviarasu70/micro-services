d055692e5de9d8050acd8d59435589292b3e3da6
kaviarasu70 <kaviarasu70@gmail.com>
2018-09-13 07:56:57 -0700

    restructure the config server

--- configservice/src/main/java/com/kavi/configservice/configservice/ConfigserviceApplication.java
+++ configservice/src/main/java/com/kavi/configservice/configservice/ConfigserviceApplication.java
@@ -2,8 +2,10 @@ package com.kavi.configservice.configservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigserviceApplication {
 
 	public static void main(String[] args) {
